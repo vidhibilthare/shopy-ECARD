303693531d6269c052a143d1f50aebf50151cecb
vidhibilthare <vidhibilthare@gmail.com>
2023-05-30 09:53:12 +0530

    add-to-cart/update

--- ecomm/app/urls.py
+++ ecomm/app/urls.py
@@ -2,22 +2,23 @@ from django.urls import path
 from app import views
 from django.conf.urls.static import static
 from django.conf import settings
-urlpatterns = [
-    # path('', views.home),
-    path('',views.ProductView.as_view(),name='home'),
-    # path('product-detail/', views.product_detail, name='product-detail'),
-    path('product-detail/<int:pk>',views.ProductDetailView.as_view(), name='product-detail'),
-    # path('cart/', views.add_to_cart, name='add-to-cart'),
-    path('add_to_cart/',views.add_to_cart,name='add_to_cart'),
-    path('cart/',views.show_cart,name='show_cart'),
-    path('buy/', views.buy_now, name='buy-now'),
-    path('profile/', views.profile, name='profile'),
-    path('address/', views.address, name='address'),
-    path('orders/', views.orders, name='orders'),
-    path('changepassword/', views.change_password, name='changepassword'),
-    path('mobile/', views.mobile, name='mobile'),
-    path('login/', views.login, name='login'),
-    path('registration/', views.customerregistration, name='customerregistration'),
-    path('checkout/', views.checkout, name='checkout'),
-]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 
+urlpatterns = [
+    path("", views.ProductView.as_view(), name="home"),
+    path(
+        "product-detail/<int:pk>",
+        views.ProductDetailView.as_view(),
+        name="product-detail",
+    ),
+    path("add-to-cart/", views.add_to_cart, name="add-to-cart"),
+    path("cart/", views.show_cart, name="show_cart"),
+    path("buy/", views.buy_now, name="buy-now"),
+    path("profile/", views.profile, name="profile"),
+    path("address/", views.address, name="address"),
+    path("orders/", views.orders, name="orders"),
+    path("changepassword/", views.change_password, name="changepassword"),
+    path("mobile/", views.mobile, name="mobile"),
+    path("login/", views.login, name="login"),
+    path("registration/", views.customerregistration, name="customerregistration"),
+    path("checkout/", views.checkout, name="checkout"),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- ecomm/app/views.py
+++ ecomm/app/views.py
@@ -1,23 +1,26 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from .models import *
 from django.views import View
 from django.db.models import Q
 
-# def home(request):
-#  return render(request, 'app/home.html')
+
 class ProductView(View):
     def get(Self, request):
-     topwears = Product.objects.filter(category='TW')
-     bottomwears = Product.objects.filter(category='BW')
-     electronics = Product.objects.filter(category='E')
-     accessories = Product.objects.filter(category='A')
-     return render(request,'app/home.html',
-                   {'topwears': topwears, 'bottomwears' : bottomwears, 'electronics': electronics, 'accessories' : accessories})
+        topwears = Product.objects.filter(category="TW")
+        bottomwears = Product.objects.filter(category="BW")
+        electronics = Product.objects.filter(category="E")
+        accessories = Product.objects.filter(category="A")
+        return render(
+            request,
+            "app/home.html",
+            {
+                "topwears": topwears,
+                "bottomwears": bottomwears,
+                "electronics": electronics,
+                "accessories": accessories,
+            },
+        )
 
-# def product_detail(request):
-#  products = Product.objects.all()
-#  context = { 'products':products}
-#  return render(request, 'app/productdetail.html',context)
 
 class ProductDetailView(View):
     def get(self, request, pk):
@@ -25,22 +28,28 @@ class ProductDetailView(View):
         item_already_in_cart = False
         if request.user.is_authenticated:
             item_already_in_cart = Cart.objects.filter(
-                Q(product=product.id) & Q(user=request.user)).exists()
-            return render(request, 'app/productdetail.html', {'product': product, 'item_already_in_cart': item_already_in_cart})
+                Q(product=product.id) & Q(user=request.user)
+            ).exists()
+            return render(
+                request,
+                "app/productdetail.html",
+                {"product": product, "item_already_in_cart": item_already_in_cart},
+            )
         else:
-            return render(request, 'app/productdetail.html', {'product': product, 'item_already_in_cart': item_already_in_cart})
-     
- 
+            return render(
+                request,
+                "app/productdetail.html",
+                {"product": product, "item_already_in_cart": item_already_in_cart},
+            )
+
 
 def add_to_cart(request):
- return render(request, 'app/addtocart.html')
+    user = request.user
+    product_id = request.GET.get("prod_id")
+    product = Product.objects.get(id=product_id)
+    Cart(user=user, product=product).save()
+    return redirect("/cart")
 
-def add_to_cart(request):
-  user = request.user
-  product_id = request.GET.get(pro_id)
-  product = Product.objects.get(id=product_id)
-  Cart(user=user,product=product).save()
-  return redirect('/cart')
 
 def show_cart(request):
     if request.user.is_authenticated:
@@ -53,46 +62,44 @@ def show_cart(request):
         print(cart_product)
         if cart_product:
             for p in cart_product:
-                tempamount = (p.quantity*p.product.discounted_price)
+                tempamount = (p.quantity*p.product.discount_price)
                 amount += tempamount
                 total_amount = amount+shipping_amount
             return render(request, 'app/addtocart.html', {'carts': cart, 'total_amount': total_amount, 'amount': amount})
         else:
             return render(request, 'app/emptycart.html')
 
-
 def buy_now(request):
- return render(request, 'app/buynow.html')
+    return render(request, "app/buynow.html")
+
 
 def profile(request):
- return render(request, 'app/profile.html')
+    return render(request, "app/profile.html")
+
 
 def address(request):
- return render(request, 'app/address.html')
+    return render(request, "app/address.html")
+
 
 def orders(request):
- return render(request, 'app/orders.html')
+    return render(request, "app/orders.html")
+
 
 def change_password(request):
- return render(request, 'app/changepassword.html')
+    return render(request, "app/changepassword.html")
+
 
 def mobile(request):
- return render(request, 'app/mobile.html')
+    return render(request, "app/mobile.html")
+
 
 def login(request):
- return render(request, 'app/login.html')
+    return render(request, "app/login.html")
+
 
 def customerregistration(request):
- return render(request, 'app/customerregistration.html')
+    return render(request, "app/customerregistration.html")
+
 
 def checkout(request):
- return render(request, 'app/checkout.html')
-
-
- 
-
-
- 
- 
- 
- 
+    return render(request, "app/checkout.html")
